75c6c15dafc4726835bff25417fdb71dbe24aa8a
LucynaPelc <lucynapelc.project@gmail.com>
2024-11-25 10:34:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -79,8 +79,3 @@ searchBox.addEventListener('input', debounce(handleSearch, 300));
 
 
 
-.catch(error => {
-    Notiflix.Notify.failure('Oops, there is no country with that name');
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
-  });
